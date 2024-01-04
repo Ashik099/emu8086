7a7736657b1049c759cbf34fcba57ad47fa3c942
Md Ashaduzzaman Ashik <ashaduzzaman099@gmail.com>
2024-01-04 13:28:45 +0600

    Create Add 2 number from user result less than 10.asm

---
+++ Add 2 number from user result less than 10.asm
@@ -0,0 +1,28 @@
+;Add 2 number from user result<10
+.model small
+.stack 100h
+.code
+
+main proc 
+   mov ah,1
+   int 21h
+   mov bl,al
+   
+   mov ah,1
+   int 21h
+   mov bh,al
+   
+   add bl,bh
+   
+   mov ah,2
+   mov dl,bl
+   sub dl,48
+   int 21h
+    
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    
+    main endp
+end main
