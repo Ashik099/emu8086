ce208b58e08ce73ef101a2e042e845f29640c57a
Md Ashaduzzaman Ashik <ashaduzzaman099@gmail.com>
2024-01-04 13:14:55 +0600

    Create x-change value.asm

---
+++ x-change value.asm
@@ -0,0 +1,28 @@
+;xchg keyword
+.model small
+.stack 100h
+.code
+
+main proc 
+    mov ah,1
+    int 21h
+    mov bl,al
+    
+    int 21h
+    mov bh,al
+    
+    xchg bl,bh
+    
+    mov ah,2
+    mov dl,bl
+    int 21h
+    
+    mov dl,bh
+    int 21h  
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    
+    main endp
+end main
