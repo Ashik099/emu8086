670efd6f58612fed5bb906ba268710a43dab05ba
Md Ashaduzzaman Ashik <ashaduzzaman099@gmail.com>
2024-01-06 23:27:23 +0600

    Create Decending order For loop using Loop Op code.asm

---
+++ Decending order For loop using Loop Op code.asm
@@ -0,0 +1,33 @@
+; Decending order For loop using Loop Op code
+.model small
+.stack 100h
+.data
+
+.code
+
+main proc 
+    mov ah,1
+    int 21h
+    mov bl,al
+    sub al,48
+    
+    mov cx,0
+    mov cl,al
+    
+    
+    
+    
+    for:
+    mov ah,2
+    mov dl,bl 
+    int 21h
+    
+    dec bl
+    
+    loop for:
+   
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
