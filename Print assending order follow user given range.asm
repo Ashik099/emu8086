5526d329614b3c6a607ffe5354c4614052f3cfad
Md Ashaduzzaman Ashik <ashaduzzaman099@gmail.com>
2024-01-06 23:28:29 +0600

    Create Print assending order follow user given range.asm

---
+++ Print assending order follow user given range.asm
@@ -0,0 +1,30 @@
+;Print assending order with range
+.model small
+.stack 100h
+.data
+
+.code
+
+main proc 
+    mov ah,1
+    int 21h
+    mov bl,al
+    
+    mov cl,'1'
+    
+    for:
+    cmp cl,bl
+    jg exit:
+    
+    mov ah,2
+    mov dl,cl
+    int 21h
+    
+    inc cl
+    jmp for:
+   
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
