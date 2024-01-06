74bca8b5d2f350b690b421e32688fdf82061fac4
Md Ashaduzzaman Ashik <ashaduzzaman099@gmail.com>
2024-01-06 23:28:25 +0600

    Create Peint asscii value.asm

---
+++ Peint asscii value.asm
@@ -0,0 +1,27 @@
+;Peint asscii value 
+.model small
+.stack 100h
+.data
+
+.code
+
+main proc 
+    mov cx,256
+    
+    mov ah,2
+    mov dl,0
+    
+    loop:
+        int 21h
+        
+        inc dl
+        dec cx
+        jnz loop
+    
+    
+   
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
